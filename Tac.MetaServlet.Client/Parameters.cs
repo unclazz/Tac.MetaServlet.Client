b654a3c9ff3847f82c0212d1ff15efa7dcb9e2f4
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-09-11 08:02:34 +0900

    クライアントのサンプルコード（作成途中）を追加

---
+++ Tac.MetaServlet.Client/Parameters.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tac.MetaServlet.Client
+{
+	public sealed class Parameters
+	{
+		private readonly List<ParameterMeta> metaList = new List<ParameterMeta>();
+
+		public string RemoteHost { get; set; }
+		public string RemotePath { get; set; }
+		public int RemotePort { get; set; }
+		public int RequestTimeout { get; set; }
+		public string RequestJson { get; set; }
+		public IList<string> Others { get; }
+
+		public Parameters()
+		{
+			Others = new List<string>();
+		}
+
+		public IEnumerable<ParameterMeta> GetMetaData()
+		{
+			if (metaList.Count == 0)
+			{
+				metaList.Add(meta("/h", "Remote.Host", (obj) => RemoteHost = obj));
+				metaList.Add(meta("/p", "Remote.Port", (obj) => RemotePort = int.Parse(obj)));
+				metaList.Add(meta("/q", "Remote.Path", (obj) => RemotePath = obj));
+				metaList.Add(meta("/t", "Request.Timeout", (obj) => RequestTimeout = int.Parse(obj)));
+				metaList.Add(meta("/j", "Request.Json", (obj) => RequestJson = obj, true));
+			}
+			return metaList.AsReadOnly();
+		}
+
+		private ParameterMeta meta(string optName, string setName, Action<string> setter, bool required)
+		{
+			return new ParameterMeta(optName, setName, setter, required);
+		}
+
+		private ParameterMeta meta(string optName, string setName, Action<string> setter)
+		{
+			return meta(optName, setName, setter, false);
+		}
+	}
+
+	public sealed class ParameterMeta
+	{
+		public string OptionName { get; }
+		public string SettingName { get; }
+		public Action<string> SetterDelegate { get; }
+		public bool Required { get; }
+
+		internal ParameterMeta(string optName, string setName, Action<string> setter, bool required)
+		{
+			if (optName == null)
+			{
+				throw new ArgumentNullException(nameof(optName));
+			}
+			if (setName == null)
+			{
+				throw new ArgumentNullException(nameof(setName));
+			}
+			if (setter == null)
+			{
+				throw new ArgumentNullException(nameof(setter));
+			}
+			OptionName = optName;
+			SettingName = setName;
+			SetterDelegate = setter;
+			Required = required;
+		}
+	}
+}
+
---
+++ Tac.MetaServlet.Client/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using Tac.MetaServlet.Json;
+using Tac.MetaServlet.Rpc;
+
+namespace Tac.MetaServlet.Client
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				new MainClass().Execute(args);
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine(e.ToString());
+			}
+		}
+
+		public void Execute(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				PrintUsage();
+				return;
+			}
+
+			var ps = ParseParameters(args);
+		}
+
+		public void PrintUsage()
+		{
+			var b = new StringBuilder()
+				.AppendLine("構文: tacrpc /j <json-file> [/h <host>] [/p <port>] [/q <path>] [/t <timeout>]")
+				.AppendLine("解説: /j  RPCリクエストを表わすJSONが記述されたファイルのパス.")
+				.AppendLine("      /h  RPCリクエスト先のホスト名. デフォルトは\"localhost\".")
+				.AppendLine("      /p  RPCリクエスト先のポート名. デフォルトは8080.")
+				.AppendLine("      /q  RPCリクエスト先のパス名. デフォルトは\"/org.talend.administrator/metaServlet\".")
+				.AppendLine("      /t  RPCリクエストのタイムアウト時間. 単位はミリ秒. デフォルトは100000.");
+			Console.WriteLine(b.ToString());
+		}
+
+		public IResponse SendRequest(Parameters ps, IJsonObject j)
+		{
+			var b = Request.Builder()
+						  .Host(ps.RemoteHost)
+						  .Port(ps.RemotePort)
+						  .Path(ps.RemotePath)
+						  .Timeout(ps.RequestTimeout);
+			j.Properties
+			 .ToList()
+			 .ForEach((obj) => b.Parameter(obj.Name, obj.Value));
+			return b.Build().Send();
+		}
+
+		public IJsonObject ParseRequestJson(Parameters ps)
+		{
+			return JsonObject.FromFile(ps.RequestJson, Encoding.UTF8);
+		}
+
+		public Parameters ParseParameters(string[] args)
+		{
+			Parameters ps = new Parameters();
+		
+			var pairs = MakeArgumentPairs(args);
+			var settings = System.Configuration.ConfigurationManager.AppSettings;
+			var result = ps.GetMetaData().Where((arg) => {
+				return !ResolveParameter(arg, pairs, settings);
+			}).Select((arg) => arg.OptionName).ToList();
+
+			if (result.Count == 0)
+			{
+				return ps;
+			}
+			else {
+				throw new ArgumentException
+				(string
+				 .Format("argument(s) muet be specified [{0}]."
+				         , result.Aggregate((arg1, arg2) => arg1 + ", " + arg2)));
+			}
+		}
+
+		private IEnumerable<Pair> MakeArgumentPairs(string[] args)
+		{
+			for (int i = 0; i + 1 < args.Length; i++)
+			{
+				yield return new Pair(args[i], args[i + 1]);
+			}
+		}
+
+		private bool ResolveParameter(ParameterMeta meta, 
+		                              IEnumerable<Pair> argPairs, 
+		                              NameValueCollection settings)
+		{
+			Pair argumentPair = argPairs
+				.FirstOrDefault((arg) => arg.Left.ToLower()
+				                .Equals(meta.OptionName));
+			string settingName = null;
+
+			if (argumentPair != null)
+			{
+				meta.SetterDelegate(argumentPair.Right);
+			}
+			else {
+				settingName = settings
+					.AllKeys
+					.FirstOrDefault((arg) => arg
+					                .Equals(meta.SettingName));
+				if (settingName != null)
+				{
+					meta.SetterDelegate(settings[settingName]);
+				}
+			}
+			return !meta.Required || argumentPair != null || settingName != null;
+		}
+
+		public sealed class Pair
+		{
+			public string Left { get; }
+			public string Right { get; }
+			internal Pair(string left, string right)
+			{
+				Left = left; Right = right;
+			}
+		}
+	}
+}
