672f9e9aee42915f7360d2e15e9f3ff26de0ad49
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-09-13 11:56:10 +0900

    コマンドライン引数に/Dと/?を追加 コマンドライン引数解析ロジックを変更

--- Tac.MetaServlet.Client/Parameters.cs
+++ Tac.MetaServlet.Client/Parameters.cs
@@ -2,61 +2,163 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Tac.MetaServlet.Client
 {
+	/// <summary>
+	/// アプリケーションのパラメータを格納するオブジェクトです.
+	/// </summary>
 	public sealed class Parameters
 	{
 		private readonly List<ParameterMeta> metaList = new List<ParameterMeta>();
 
+		/// <summary>
+		/// リクエスト先のホスト名を取得・設定します.
+		/// </summary>
+		/// <value>ホスト名（もしくはIPアドレス）.</value>
 		public string RemoteHost { get; set; }
+		/// <summary>
+		/// リクエスト先のパス名を取得・設定します.
+		/// </summary>
+		/// <value>パス名.</value>
 		public string RemotePath { get; set; }
+		/// <summary>
+		/// リクエスト先のポート番号を取得・設定します.
+		/// </summary>
+		/// <value>リクエスト先のポート番号.</value>
 		public int RemotePort { get; set; }
+		/// <summary>
+		/// リクエストのタイムアウト時間を取得・設定します.
+		/// </summary>
+		/// <value>タイムアウト時間（ミリ秒単位）.</value>
 		public int RequestTimeout { get; set; }
+		/// <summary>
+		/// リクエスト内容となるJSONのパスを取得・設定します.
+		/// </summary>
+		/// <value>JSONのパス.</value>
 		public string RequestJson { get; set; }
-		public IList<string> Others { get; }
+		/// <summary>
+		/// ヘルプ表示フラグを取得・設定します.
+		/// </summary>
+		/// <value>ヘルプ表示フラグ.</value>
+		public bool ShowHelp { get; set; }
+		/// <summary>
+		/// ダンプ出力フラグを取得・設定します.
+		/// </summary>
+		/// <value>ダンプ出力フラグ.</value>
+		public bool ShowDump { get; set; }
 
+		/// <summary>
+		/// インスタンスを生成し各プロパティをデフォルト値とともに初期化します.
+		/// </summary>
 		public Parameters()
 		{
             RemoteHost = "localhost";
             RemotePath = "/org.talend.administrator/metaServlet";
             RemotePort = 8080;
             RequestTimeout = 100000;
-			Others = new List<string>();
+			ShowDump = false;
+			ShowHelp = false;
 		}
 
+		/// <summary>
+		/// 構文解説テキストを生成して返します.
+		/// </summary>
+		/// <returns>構文解説テキスト.</returns>
+		public static string GetSyntax()
+		{
+			var b = new StringBuilder()
+				.AppendLine("TACRPC {/J <json-file> | /?} [/H <host>] [/P <port>] [/Q <path>] [/T <timeout>] [/D]")
+				.AppendLine("/J  RPCリクエストを表わすJSONが記述されたファイルのパス.")
+				.AppendLine("/H  RPCリクエスト先のホスト名. デフォルトは\"localhost\".")
+				.AppendLine("/P  RPCリクエスト先のポート名. デフォルトは8080.")
+				.AppendLine("/Q  RPCリクエスト先のパス名. デフォルトは\"/org.talend.administrator/metaServlet\".")
+				.AppendLine("/T  RPCリクエストのタイムアウト時間. 単位はミリ秒. デフォルトは100000.")
+				.AppendLine("/D  リクエストとレスポンスのダンプ出力を行う.")
+				.AppendLine("/?  このヘルプを表示する.");
+
+			return b.ToString();
+		}
+
+		/// <summary>
+		/// パラメータの妥当性をチェックします.
+		/// </summary>
+		/// <exception cref="ArgumentException">必須パラメータの欠如や妥当でないパラメータの存在が検知された場合</exception>
+		public void Validate()
+		{
+			if (ShowHelp) return;
+			if (RequestJson == null || RequestJson.Length == 0)
+			{
+				throw new ArgumentException("コマンドライン引数 /J の指定が必要です.");
+			}
+			if (!File.Exists(RequestJson))
+			{
+				throw new ArgumentException("コマンドライン引数 /J に指定されたパスは存在しません.");
+			}
+			if (RequestTimeout < 0)
+			{
+				throw new ArgumentException("コマンドライン引数 /T には正の整数を指定してください.");
+			}
+			if (RemotePort <= 0)
+			{
+				throw new ArgumentException("コマンドライン引数 /P には正の整数を指定してください.");
+			}
+		}
+
+		/// <summary>
+		/// パラメータのメタ情報のシーケンスを取得します.
+		/// </summary>
+		/// <returns>メタデータのシーケンス.</returns>
         public IEnumerable<ParameterMeta> GetMetaData()
 		{
 			if (metaList.Count == 0)
 			{
-                metaList.Add(meta("/J", "Request.Json", (obj) => RequestJson = obj, true));
-                metaList.Add(meta("/H", "Remote.Host", (obj) => RemoteHost = obj));
-				metaList.Add(meta("/P", "Remote.Port", (obj) => RemotePort = int.Parse(obj)));
-				metaList.Add(meta("/Q", "Remote.Path", (obj) => RemotePath = obj));
-				metaList.Add(meta("/T", "Request.Timeout", (obj) => RequestTimeout = int.Parse(obj)));
+                metaList.Add(meta("/J", "Tac.MetaServlet.Client.Request.Json", (obj) => RequestJson = obj));
+                metaList.Add(meta("/H", "Tac.MetaServlet.Client.Remote.Host", (obj) => RemoteHost = obj));
+				metaList.Add(meta("/P", "Tac.MetaServlet.Client.Remote.Port", (obj) => RemotePort = int.Parse(obj)));
+				metaList.Add(meta("/Q", "Tac.MetaServlet.Client.Remote.Path", (obj) => RemotePath = obj));
+				metaList.Add(meta("/T", "Tac.MetaServlet.Client.Request.Timeout", (obj) => RequestTimeout = int.Parse(obj)));
+				metaList.Add(meta("/D", "Tac.MetaServlet.Client.Show.Dump", (obj) => ShowDump = true));
+				metaList.Add(meta("/?", "Tac.MetaServlet.Client.Show.Help", (obj) => ShowHelp = true));
 			}
 			return metaList.AsReadOnly();
 		}
 
-		private ParameterMeta meta(string optName, string setName, Action<string> setter, bool required)
-		{
-			return new ParameterMeta(optName, setName, setter, required);
-		}
-
 		private ParameterMeta meta(string optName, string setName, Action<string> setter)
 		{
-			return meta(optName, setName, setter, false);
+			return new ParameterMeta(optName, setName, setter);
 		}
 	}
 
+	/// <summary>
+	/// パラメータのメタ情報を表わすオブジェクトです.
+	/// </summary>
 	public sealed class ParameterMeta
 	{
+		/// <summary>
+		/// コマンドラインオプションとしての名前を取得します.
+		/// </summary>
+		/// <value>オプション名.</value>
 		public string OptionName { get; }
+		/// <summary>
+		/// アプリケーション構成ファイルの設定情報としての名前を取得します.
+		/// </summary>
+		/// <value>設定情報名.</value>
 		public string SettingName { get; }
+		/// <summary>
+		/// パラメータを格納するオブジェクトに値を設定するためのデリゲートを取得します.
+		/// </summary>
+		/// <value>デリゲート.</value>
 		public Action<string> SetterDelegate { get; }
-		public bool Required { get; }
 
-		internal ParameterMeta(string optName, string setName, Action<string> setter, bool required)
+		/// <summary>
+		/// インスタンスを生成し引数の妥当性を検証します.
+		/// </summary>
+		/// <param name="optName">オプション名.</param>
+		/// <param name="setName">設定情報名.</param>
+		/// <param name="setter">setterデリゲート.</param>
+		internal ParameterMeta(string optName, string setName, Action<string> setter)
 		{
 			if (optName == null)
 			{
@@ -73,7 +175,6 @@ namespace Tac.MetaServlet.Client
 			OptionName = optName;
 			SettingName = setName;
 			SetterDelegate = setter;
-			Required = required;
 		}
 	}
 }
--- Tac.MetaServlet.Client/Program.cs
+++ Tac.MetaServlet.Client/Program.cs
@@ -9,73 +9,162 @@ using Tac.MetaServlet.Rpc;
 
 namespace Tac.MetaServlet.Client
 {
+	/// <summary>
+	/// アプリケーションのメインモジュールです.
+	/// </summary>
     class MainClass
     {
-        public static int Main(string[] args)
+		private static readonly int ExitCodeNormal = 0;
+		private static readonly int ExitCodeAbnormal = 1;
+		private static readonly IJsonFormatOptions formatOptions =
+			JsonFormatOptions.Builder().Indent(true).Build();
+
+		/// <summary>
+		/// アプリケーションのエントリーポイントです.
+		/// </summary>
+		/// <param name="args">コマンドライン引数の文字列.</param>
+		/// <returns>アプリケーションの終了コード（正常終了であれば<c>0</c>、異常終了であれば<c>1</c>）.</returns>
+		public static int Main(string[] args)
+        {
+            return new MainClass().Execute(args);
+        }
+
+        public int Execute(string[] args)
         {
             try
             {
-                new MainClass().Execute(args);
+				// コマンドライン引数からパラメータを読み取る
+                var ps = ParseParameters(args);
+				// ヘルプ表示のフラグが指定されていた場合はヘルプを表示し終了
+				if (ps.ShowHelp)
+				{
+					PrintMessage("構文", Parameters.GetSyntax());
+					return ExitCodeNormal;
+				}
+
+				// パラメータ情報を元にリクエスト内容となるJSONを読み取る
+				var json = ParseRequestJson(ps);
+				// パラメータとJSONからリクエストを構築する
+                var req = BuildRequest(ps, json);
+				// ダンプフラグが指定されていた場合はリクエスト内容をダンプ
+				if (ps.ShowDump)  DumpRequest(req);
+				// リクエストを同期的に送信してレスポンスを得る
+                var resp = req.Send();
+				// ダンプフラグをチェック
+				if (ps.ShowDump)
+				{
+					// 指定されていた場合はレスポンス内容をダンプ
+					DumpResponse(resp);
+				}
+				else {
+					// 指定されていなかった場合はレスポンス本文のみをダンプ
+					PrintMessage(string.Empty, resp.Body);
+				}
+				return ExitCodeNormal;
             }
             catch (Exception e)
             {
-                Console.Error.Write("エラー: ");
-                Console.Error.WriteLine(e.ToString());
-                return 1;
-            }
-            return 0;
-        }
-
-        private readonly IJsonFormatOptions formatOptions =
-            JsonFormatOptions.Builder().Indent(true).Build();
-
-        public void Execute(string[] args)
-        {
-            try
-            {
-                var ps = ParseParameters(args);
-                var j = ParseRequestJson(ps);
-                var req = BuildRequest(ps, j);
-                DumpRequest(req);
-                var resp = req.Send();
-                DumpResponse(resp);
-            }
-            catch (ArgumentException e)
-            {
-                PrintUsage();
-                throw;
+				if (e is ArgumentException)
+				{
+					// 引数例外の場合はヘルプを表示する
+					PrintErrorMessage("構文", Parameters.GetSyntax());
+				}
+				// エラー内容を表示する
+				PrintErrorMessage("エラー", e);
+				return ExitCodeAbnormal;
             }
         }
 
-        public void DumpRequest(IRequest req)
+		/// <summary>
+		/// RPCリクエストを標準出力にダンプします.
+		/// </summary>
+		/// <param name="req">レスポンス.</param>
+		public void DumpRequest(IRequest req)
         {
             var b = JsonObject.Builder(req.Parameters);
             if (req.Parameters.HasProperty("authPass"))
             {
                 b.Append("authPass", "*****");
             }
-            Console.WriteLine("リクエストURI : {0}", req.Uri);
-            Console.WriteLine("リクエストJSON: {0}", b.Build().Format(formatOptions));
+			PrintMessage("リクエストURI", req.Uri);
+			PrintMessage("リクエストJSON", b.Build().Format(formatOptions));
         }
 
+		/// <summary>
+		/// RPCレスポンスを標準出力にダンプします.
+		/// </summary>
+		/// <param name="resp">レスポンス.</param>
         public void DumpResponse(IResponse resp)
         {
-            Console.WriteLine("HTTPステータス: {0}", resp.StatusCode);
-            Console.WriteLine("レスポンスJSON: {0}", resp.Body.Format(formatOptions));
+            PrintMessage("HTTPステータス", resp.StatusCode);
+            PrintMessage("レスポンスJSON", resp.Body.Format(formatOptions));
         }
 
-        public void PrintUsage()
-        {
-            var b = new StringBuilder()
-                .AppendLine("構文: TACRPC /J <json-file> [/H <host>] [/P <port>] [/Q <path>] [/T <timeout>]")
-                .AppendLine("解説: /J  RPCリクエストを表わすJSONが記述されたファイルのパス.")
-                .AppendLine("      /H  RPCリクエスト先のホスト名. デフォルトは\"localhost\".")
-                .AppendLine("      /P  RPCリクエスト先のポート名. デフォルトは8080.")
-                .AppendLine("      /Q  RPCリクエスト先のパス名. デフォルトは\"/org.talend.administrator/metaServlet\".")
-                .AppendLine("      /T  RPCリクエストのタイムアウト時間. 単位はミリ秒. デフォルトは100000.");
-            Console.WriteLine(b.ToString());
-        }
+		/// <summary>
+		/// 標準出力にメッセージを出力します.
+		/// </summary>
+		/// <param name="label">ラベル.</param>
+		/// <param name="target">メッセージに使用されるオブジェクト.</param>
+		public void PrintMessage(string label, object target)
+		{
+			Console.WriteLine(FormatMessage(label, target));
+		}
 
+		/// <summary>
+		/// 標準エラー出力にメッセージを出力します.
+		/// </summary>
+		/// <param name="label">ラベル.</param>
+		/// <param name="target">メッセージに使用されるオブジェクト.</param>
+		public void PrintErrorMessage(string label, object target)
+		{
+			Console.Error.WriteLine(FormatMessage(label, target));
+		}
+
+		/// <summary>
+		/// コンソール出力するためにメッセージを整形します.
+		/// ラベルとして空文字列もしくは<c>null</c>が指定された場合は、
+		/// ターゲットとなるオブジェクトの文字列表現を返すだけでとくに整形を行いません。
+		/// </summary>
+		/// <returns>整形済みのメッセージ.</returns>
+		/// <param name="label">ラベル.</param>
+		/// <param name="target">メッセージに使用されるオブジェクト.</param>
+		public string FormatMessage(string label, object target)
+		{
+			if (label == null || label.Length == 0)
+			{
+				return target.ToString();
+			}
+
+			// ラベルの幅を計算し（日本語環境を前提としている）
+			// その幅でもって出力の2行目以降インデントするための空白文字シーケンスを準備する
+			var shiftJis = Encoding.GetEncoding("Shift_JIS");
+			var labelWidth = shiftJis.GetByteCount(label);
+			var labelColon = new StringBuilder(label).Append(": ");
+			var indent = Enumerable.Range(0, labelWidth)
+					  .Aggregate(new StringBuilder(), (b, s) => b.Append(' '));
+
+			// 出力対象のオブジェクトを一度文字列化
+			// その結果を改行文字で行ごとに分解し（空文字列は排除する）
+			// 1行目の先頭にはラベルを付加し、2行目以降の先頭にはインデントを付加する
+			var buff = target
+				.ToString()
+				.Split(new char[] {'\n', '\r'},
+				       StringSplitOptions.RemoveEmptyEntries)
+				.Aggregate(new StringBuilder(),
+				           (b, s) => b
+				           .Append(b.Length == 0 ? labelColon : indent)
+				           .AppendLine(s));
+
+			// 最終行に余計な改行文字列が入るのでこれを除去してから呼び出し元に返す
+			return buff.ToString().TrimEnd();
+		}
+
+		/// <summary>
+		/// RPCリクエストを構築します.
+		/// </summary>
+		/// <returns>構築した結果.</returns>
+		/// <param name="ps">パラメータが格納されたオブジェクト.</param>
+		/// <param name="j">リクエスト内容となるJSON.</param>
         public IRequest BuildRequest(Parameters ps, IJsonObject j)
         {
             var b = Request.Builder()
@@ -89,78 +178,86 @@ namespace Tac.MetaServlet.Client
             return b.Build();
         }
 
+		/// <summary>
+		/// RPCリクエストの内容となるJSONを読み取ります.
+		/// </summary>
+		/// <returns>パース結果.</returns>
+		/// <param name="ps">パラメータが格納されたオブジェクト.</param>
         public IJsonObject ParseRequestJson(Parameters ps)
         {
             return JsonObject.FromFile(ps.RequestJson, Encoding.UTF8);
         }
 
+		/// <summary>
+		/// パラメータを読み取ります.
+		/// </summary>
+		/// <returns>読み取った結果.</returns>
+		/// <param name="args">コマンドライン引数の文字列配列.</param>
         public Parameters ParseParameters(string[] args)
         {
             Parameters ps = new Parameters();
 
-            var pairs = MakeArgumentPairs(args);
             var settings = System.Configuration.ConfigurationManager.AppSettings;
-            var result = ps.GetMetaData().Where((arg) =>
-            {
-                return !ResolveParameter(arg, pairs, settings);
-            }).Select((arg) => arg.OptionName).ToList();
+			ps.GetMetaData().ToList().ForEach((meta) => ResolveParameter(meta, args, settings));
 
-            if (result.Count == 0)
-            {
-                return ps;
-            }
-            else
-            {
-                throw new ArgumentException
-                (string
-                 .Format("argument(s) must be specified [{0}]."
-                         , result.Aggregate((arg1, arg2) => arg1 + ", " + arg2)));
-            }
+			ps.Validate();
+
+			return ps;
         }
 
-        private IEnumerable<Pair> MakeArgumentPairs(string[] args)
-        {
-            for (int i = 0; i + 1 < args.Length; i++)
-            {
-                yield return new Pair(args[i], args[i + 1]);
-            }
-        }
+		/// <summary>
+		/// パラメータを解決します.
+		/// このメソッドは<see cref="ParameterMeta"/>インスタンスと
+		/// コマンドライン引数の配列、そしてアプリケーション構成ファイルで定義された設定情報をもとに、
+		/// アプリケーションのパラメータを解決して、パラメータ値を<see cref="Parameters"/>に設定します。
+		/// </summary>
+		/// <returns>パラメータの解決に成功した場合<c>true</c>.</returns>
+		/// <param name="meta">パラメータのメタ情報.</param>
+		/// <param name="args">コマンドライン引数の文字列配列.</param>
+		/// <param name="settings">アプリケーション構成ファイルの設定情報.</param>
+		public bool ResolveParameter(ParameterMeta meta,
+				string[] args, NameValueCollection settings)
+		{
+			// コマンドライン引数のペアを作るための一時変数
+			// 要素位置を1だけずらした2つのシーケンスを用意する
+			// 例：
+			// argsLeft  ["/J", "foo/bar/baz.json", "/H", "127.0.0.1"]
+			// argsRight ["foo/bar/baz.json", "/H", "127.0.0.1", ""]
+			// この2つをzip処理すると [("/J", "foo/bar/baz.json"), ...] というペアのシーケンスが得られる
+			IEnumerable<string> argsLeft = args;
+			IEnumerable<string> argsRight = args.Skip(1).Concat(new string[1]{ "" });
 
-        private bool ResolveParameter(ParameterMeta meta,
-                                      IEnumerable<Pair> argPairs,
-                                      NameValueCollection settings)
-        {
-            Pair argumentPair = argPairs
-                .FirstOrDefault((arg) => arg.Left.ToUpper()
-                                .Equals(meta.OptionName));
-            string settingName = null;
+			// アプリケーション構成ファイルで定義された設定情報のうち
+			// 特定の名称を持つものを抽出してパラメータを表わす匿名型のインスタンスでラップする
+			var sets = settings.AllKeys
+				.Where((a) => a.Equals(meta.SettingName))
+				.Select((a) => new { Name = a, Value = settings[a], Priority = 2 });
 
-            if (argumentPair != null)
-            {
-                meta.SetterDelegate(argumentPair.Right);
-            }
-            else
-            {
-                settingName = settings
-                    .AllKeys
-                    .FirstOrDefault((arg) => arg
-                                    .Equals(meta.SettingName));
-                if (settingName != null)
-                {
-                    meta.SetterDelegate(settings[settingName]);
-                }
-            }
-            return !meta.Required || argumentPair != null || settingName != null;
-        }
+			// Zip    : コマンドライン引数のペアを作成しパラメータを表わす匿名型のインスタンスでラップする
+			// Where  : その中で特定の名称を持つものだけを抽出して
+			// Concat : アプリケーション構成ファイル由来のパラメータの結果セットと集合和を構成する
+			// OrderBy: 優先度にしたがい結果セットをソート
+			// FirstOrDefault: 結果セットの先頭の（最初の）要素を取得（シーケンスが空ならnullを取得）
+			var param = argsLeft
+				.Zip(argsRight, (a1, a2) => new { Name = a1, Value = a2, Priority = 1 })
+				.Where((a) => a.Name.ToUpper().Equals(meta.OptionName))
+				.Concat(sets)
+				.OrderBy((a) => a.Priority)
+				.FirstOrDefault();
 
-        public sealed class Pair
-        {
-            public string Left { get; }
-            public string Right { get; }
-            internal Pair(string left, string right)
-            {
-                Left = left; Right = right;
-            }
-        }
+			// パラメータ（匿名型インスタンスで表される）の解決に成功したかどうかチェック
+			if (param != null)
+			{
+				// 解決に成功した場合はそのパラメータ値をバリューオブジェクトに格納
+				meta.SetterDelegate(param.Value);
+				// 呼び出し元にパラメータ解決に成功した旨通知
+				return true;
+			}
+			else
+			{
+				// パラメータ解決に失敗した旨通知
+				return false;
+			}
+		}
     }
 }
