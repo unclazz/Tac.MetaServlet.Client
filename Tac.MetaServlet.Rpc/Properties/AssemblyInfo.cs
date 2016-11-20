44670c2ae81e54a3db37c62cb52ec9af15ea04f7
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-11-20 11:00:36 +0900

    NuGetパッケージングのためのメタ情報を追加

--- Tac.MetaServlet.Rpc/Properties/AssemblyInfo.cs
+++ Tac.MetaServlet.Rpc/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Tac.MetaServlet.Rpc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("RPC client assebmly for Talend Administration Center (TAC)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("unclazz.org")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
